8be8ae06ae6e6dbb7b69ba3754821482e4696707
SilverAlexTV <41821968+SilverAlexTV@users.noreply.github.com>
2019-03-08 17:39:34 +0100

    update

---
+++ apprentissage-php/script.php
@@ -0,0 +1,8 @@
+<?php
+set_time_limit(0);      /*permet au script de s'exécuter indéfiniment */
+while (1)
+{
+
+    sleep(300);
+}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo '<a href="site-final/index.php">Site final</a>';
+echo '<a href="maquette/index.php">Maquette</a>';
+echo '<a href="apprentissage-php">Apprentissage PHP</a>';
+?>
