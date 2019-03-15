547b37798fd73196a7f4d5b4d900c37bf2015148
SilverAlexTV <41821968+SilverAlexTV@users.noreply.github.com>
2019-03-15 09:35:41 +0100

    update

---
+++ apprentissage-php/index.php
@@ -0,0 +1,5 @@
+<?php
+echo '<p><a href="TP_mini_chat/minichat.php">TP Minichat</a></p>';
+echo '<p><a href="TP_page_protegee_par_mot_de_passe/formulaire.php">Tp page protégéé par mot de passe</a></p>';
+echo '<p><a href="contact.php">Test fonctionnement formulaire de contact</a></p>';
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo '<p><a href="site-final/index.php">Site final</a></p>';
 echo '<p><a href="maquette/index.html">Maquette</a></p>';
-echo '<p><a href="apprentissage-php">Apprentissage PHP</a></p>';
+echo '<p><a href="apprentissage-php/index.php">Apprentissage PHP</a></p>';
 ?>
