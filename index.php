a97335bb67ce4f9bab357879974e49fdc01dc96c
SilverAlexTV <41821968+SilverAlexTV@users.noreply.github.com>
2019-03-15 09:13:41 +0100

    update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo '<p><a href="site-final/index.php">Site final</a></p>';
-echo '<p><a href="maquette/index.php">Maquette</a></p>';
+echo '<p><a href="maquette/index.html">Maquette</a></p>';
 echo '<p><a href="apprentissage-php">Apprentissage PHP</a></p>';
 ?>
